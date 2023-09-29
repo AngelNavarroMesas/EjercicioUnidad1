f59cef6170d3ac550b1e627f7425300d7735043a
anavarro <anavarro@107-09>
2023-09-29 11:26:29 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundoVB_WF/Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports Biblioteca
+
+Public Class Form1
+
+    Dim persona As New Biblioteca.ClsPersona
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSaludar.Click
+        persona.Nombre = txtNombre.Text
+
+        MessageBox.Show($"hola {persona.Nombre}")
+    End Sub
+
+End Class
